6a1f2bd9b64fb6fd024206da0a388c39e6211bec
lbran3 <latavia.branson@gmail.com>
2024-08-30 12:56:12 -0400

    phraseomatic2

---
+++ src/main/java/phraseomatic2/Hw2.java
@@ -0,0 +1,22 @@
+package phraseomatic2;
+
+public class Hw2 {
+
+        public static void main(String[] args) {
+            String[] wordListOne = {"pervasive", "30,00 foot", "B-to-B"};
+            String[] wordListTwo = {"empowered", "oriented", "distibuted"};
+            String[] wordListThree = {"mission", "mindshare", "space"};
+
+            int oneLength = wordListOne.length;
+            int twoLength = wordListTwo.length;
+            int threeLength = wordListThree.length;
+
+            int rand1 = (int) (Math.random() * oneLength);
+            int rand2 = (int) (Math.random() * twoLength);
+            int rand3 = (int) (Math.random() * threeLength);
+
+            String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
+
+            System.out.println("What we need is a " + phrase);
+        }
+    }
